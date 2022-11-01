29baae45b8ef299011007ff245eba341fccfdb38
Rashmi <rashmi.rrp@gmail.com>
2022-11-01 12:59:27 +0530

    first commit

---
+++ src/main/java/com/spring/jenkins/demo/SpringJenkinsApplication.java
@@ -0,0 +1,24 @@
+package com.spring.jenkins.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+    public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
+    @PostConstruct
+    public void inIt() {
+        logger.info("Application Started...");
+    }
+
+    public static void main(String[] args) {
+        logger.info("Application Executed...");
+        SpringApplication.run(SpringJenkinsApplication.class, args);
+    }
+
+}
