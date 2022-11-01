e586d7356e7af678e249e605de874ccf0202010a
Rashmi <rashmi.rrp@gmail.com>
2022-11-01 23:17:30 +0530

    Second commit

--- src/test/java/com/spring/jenkins/demo/SpringJenkinsApplicationTests.java
+++ src/test/java/com/spring/jenkins/demo/SpringJenkinsApplicationTests.java
@@ -1,10 +1,14 @@
 package com.spring.jenkins.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 Logger logger= LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
@@ -12,6 +16,8 @@ Logger logger= LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		logger.info("Test cas executing....");
+		assertEquals(true,true);
 	}
 
 }
